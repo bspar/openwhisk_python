0da1d2546fe6cf84a78f01cba3d1b7c34ed386ee
cclauss <cclauss@bluewin.ch>
2017-03-11 00:09:53 +0100

    Update and rename test_datatypes.py to test_data_types.py

--- test_datatypes.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-
-def main(t_str='t_str', t_int=1, t_float=1.2, t_list=[0, 1], t_tuple=(0, 1), t_dict={'a': 0, 'b': 1}):
-    return locals()
-
-
-if __name__ == '__main__':
-    print(main())
