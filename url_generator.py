52b1c265da742fbcb845b465114f50776dbf005a
cclauss <cclauss@bluewin.ch>
2017-03-10 17:04:41 +0100

    Update url_generator.py

--- url_generator.py
+++ url_generator.py
@@ -8,10 +8,10 @@
   Examples:
      $ python3
      >>> import url_generator
-     >>> gen = url_generator.UrlGenerator('openwhisk.ng.bluemix.net')
-     >>> print(gen.url(kind='action', name='my_action'))
-     >>> print(gen.url('aCtIoNs', 'my_action', verb='invoke', 'message'='Hi!'))
-     >>> print(whisk.action_invoke('submit_order'))
+     >>> gen = url_generator.UrlGenerator(api_host='openwhisk.ng.bluemix.net')
+     >>> print(gen.url_action('my_action'))
+     >>> gen.package = 'my_package'
+     >>> print(gen.url_action('my_action', blocking=True, result=True))
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -36,7 +36,23 @@ except ImportError:
     from urllib import urlencode        # Python 2
 
 
-class UrlGenerator(dict):
+def url_finish(*args, **kwargs):
+    """completes the right side of a url.
+       URLs for REST APIs are composed of three parts:
+       1. Base URL: https://openwhisk.ng.bluemix.net/api/v1/namespaces/_
+       2. Path Parameters (args) separated by /'s
+       3. Query Parameters (kwargs) started with a ? and then urlencoded
+       args are a tuple of unnamed values that go on the left of the ?
+       kwargs are a dict of named values that are urlencoded to right of ?"""
+    s = ''
+    if args:
+        s = '/' + '/'.join(str(arg) for arg in args)
+    if kwargs:
+        s += '?' + urlencode(kwargs)
+    return s
+
+
+class UrlGenerator(object):
     """https://console.ng.bluemix.net/docs/openwhisk/openwhisk_reference.html
        https://console.ng.bluemix.net/apidocs/98-ibm-bluemix-openwhisk"""
 
@@ -45,105 +61,69 @@ class UrlGenerator(dict):
         self.url_whisk_system = self.url_base + '/whisk.system/packages'
         self.url_whisk_utils = self.url_base + '/whisk.system/actions/utils'
         self._package = ''
-        self.kinds = {'action': self._actions,
-                      'activation': self._activations,
-                      'package': self._packages,
-                      'rule': self._rules,
-                      'trigger': self._triggers}
-
-    # Dynamic URLs that change as self.package changes
-    def url(self, kind, name='', verb='', **kwargs):
-        kind = str(kind or '').strip().lower().rstrip('s')
-        try:
-            url = self.kinds[kind]()  # lookup the method and then call it
-        except KeyError as e:
-            print(kind + ' is not supported.')
-            print(e)
-            raise
-        name = str(name or '').strip()
-        if name:
-            url += '/{}'.format(name)
-            verb = str(verb or '').strip().lower()
-            if verb in 'create delete invoke update'.split():
-                url += '/{}'.format(verb)
-        if kwargs:
-            url += '?' + urlencode(kwargs)
-        return url
 
     @property
     def package(self):
-        """Append '/packages/{package}' to URLs only if self.package is set."""
-        return ('/packages/' + self._package) if self._package else ''
+        return self._package
 
     @package.setter
     def package(self, package_name):
         self._package = str(package_name or '').strip()
 
-    # @property
-    def _current_package(self):
+    @property
+    def _curr_package(self):
         """URL is built dynamically using the current self.namespace.
            https://{host}/api/v1/namespaces/-[/packages/{package}]"""
-        return self.url_base + self.package
+        return self.url_base + ('/packages/' +
+                                self._package) if self._package else ''
 
-    # @property
-    def _actions(self):
-        """URL is built dynamically using url_current_package.
-           https://{host}/api/v1/namespaces/-/[/packages/{package}]/actions"""
-        return self._current_package() + '/actions'
+    def url_package(self, *args, **kwargs):
+        return self.url_base + '/packages' + url_finish(*args, **kwargs)
 
-    # @property
-    def _activations(self):
-        """URL is built dynamically using url_current_package.
-           https://{host}/api/v1/namespaces/-/[/packages/{package}]/activations
-           """
-        return self._current_package() + '/activations'
+    def url_action(self, *args, **kwargs):
+        return self._curr_package + '/actions' + url_finish(*args, **kwargs)
 
-    # @property
-    def _packages(self):
-        """URL is built dynamically using url_current_package.
-               https://{host}/api/v1/namespaces/-/packages"""
-        return self.url_base + '/packages'
+    def url_activation(self, *args, **kwargs):
+        return self._curr_package + '/activations' + url_finish(*args,
+                                                                **kwargs)
 
-    # @property
-    def _rules(self):
-        """URL is built dynamically using url_current_package.
-           https://{host}/api/v1/namespaces/-/[/packages/{package}]/rules"""
-        return self._current_package() + '/rules'
+    def url_rule(self, *args, **kwargs):
+        return self._curr_package + '/rules' + url_finish(*args, **kwargs)
 
-    # @property
-    def _triggers(self):
-        """URL is built dynamically using url_current_package.
-           https://{host}/api/v1/namespaces/-/[/packages/{package}]/triggers"""
-        return self._current_package() + '/triggers'
+    def url_trigger(self, *args, **kwargs):
+        return self._curr_package + '/triggers' + url_finish(*args, **kwargs)
 
 
 if __name__ == '__main__':
-    kinds = 'AcTiOnS action ACTIVATION packages RULESSS rule tRiGgErSs'.split()
     gen = UrlGenerator()
-    print('\n'.join('{:>10}: {}'.format(k, gen.url(k)) for k in kinds))
+    print(url_finish('0', 1, 2.0, a=0, b=1, c=2.0))
+
+    kinds = {'action': gen.url_action,
+             'activation': gen.url_activation,
+             'package': gen.url_package,
+             'rule': gen.url_rule,
+             'trigger': gen.url_trigger}
+
+    print('\n'.join('{:>10}: {}'.format(key, func())
+                    for key, func in kinds.items()))
 
     print(gen.package)
     gen.package = 'PaCkAgE'
     print(gen.package)
-    print('\n'.join('{:>10}: {}'.format(k, gen.url(k)) for k in kinds))
+    print('\n'.join('{:>10}: {}'.format(key, func())
+                    for key, func in kinds.items()))
 
     print('')
     gen.package = 'name_test'
     print(gen.package)
-    print('\n'.join('{:>10}: {}'.format(k, gen.url(k, 'name')) for k in kinds))
+    print('\n'.join('{:>10}: {}'.format(key, func('name'))
+                    for key, func in kinds.items()))
 
     print('')
     gen.package = 'args_test'
     print(gen.package)
-    print('\n'.join('{:>10}: {}'.format(k, gen.url(kind=k, name='args', a=-1.2,
-                                                   b=False, c='Open ?/& Whisk',
-                                                   d={'e': 'Hi', 'f': [0, 1]}))
-                    for k in kinds))
-
-    print('')
-    gen.package = 'invoke_test'
-    print(gen.package)
-    print('\n'.join('{:>10}: {}'.format(k, gen.url(k, 'name', verb='InVoKe',
-                                                   a=69, b=True, c='OpenWhisk',
-                                                   d={'e': 'Hi', 'f': [0, 1]}))
-                    for k in kinds))
+    print('\n'.join('{:>10}: {}'.format(key, func('path0', 'path1', 'path2',
+                                                  query0=-1.2, query1=False,
+                                                  query2='Open ?/& Whisk',
+                                                  d={'e': 'Hi', 'f': [0, 1]}))
+                    for key, func in kinds.items()))
